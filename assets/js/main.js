656644fe7ee9bd2ea0cf48d98c7eea23bb58694a
freelancersazal <freelancersazal@gmail.com>
2023-12-05 20:06:40 +0600

    home 2 updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ $(function () {
 
 
 
-    //======category slider======
+    //======destination slider======
     $('.destination_slider').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
@@ -85,6 +85,72 @@ $(function () {
     $(".adv_search_close").on("click", function () {
         $(".adv_search_area").removeClass("show_search");
     });
+
+
+    //======category slider======
+    $('.category_pro_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        cssEase: 'linear',
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="fas fa-long-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="fas fa-long-arrow-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+    //====== testimonial slider ======= 
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        focusOnSelect: true
+    });
+
+
 
 
 
