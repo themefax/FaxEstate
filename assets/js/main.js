62f0fbe2a20cdd53af5afe5f7c8fa75486a8afd4
freelancersazal <freelancersazal@gmail.com>
2023-12-08 03:14:49 +0600

    home 3 updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,6 +151,102 @@ $(function () {
     });
 
 
+    //======category slider======
+    $('.latest_pro_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+    //======featured listingr slider======
+    $('.featured_listing_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="fas fa-long-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="fas fa-long-arrow-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+
+    //======testimonial 2 slider======
+    $('.testimonial_2_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true,
+        nextArrow: '<i class="fas fa-long-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="fas fa-long-arrow-left prevArrow"></i>',
+    });
+
+
+
+
 
 
 
